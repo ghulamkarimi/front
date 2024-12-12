ea31c6111297bbba609c900191f4e18f6a2db67f
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-12-12 21:38:12 +0100

    Refactor vehicle rental page to use dynamic import for CarSearch component and improve code structure

--- src/app/fahrzeugvermietung/page.tsx
+++ src/app/fahrzeugvermietung/page.tsx
@@ -1,25 +1,17 @@
-'use client'
+'use client';
 
-import CarSearch from "@/components/car-form/CarSearch";
+import dynamic from 'next/dynamic';
 
+const CarSearch = dynamic(() => import('@/components/car-form/CarSearch'), { ssr: false });
 
+const Page = () => {
+  return (
+    <div className="mx-w-full flex justify-center relative mt-2">
+      <div className="xl:w-2/3 w-full relative overflow-hidden">
+        <CarSearch />
+      </div>
+    </div>
+  );
+};
 
-const page = () => {
-
- 
-   
-    return (
-        <div className={`mx-w-full flex justify-center relative mt-2 `}>
-            
-         
-            <div className={`xl:w-2/3 w-full relative overflow-hidden`}>
-                <CarSearch />
-            </div>
-
-           
-           
-        </div>
-    );
-}
-
-export default page;
+export default Page;
